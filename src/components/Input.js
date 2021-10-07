cf706c506c96b277f4ea59bec3aa0b7be256a508
Basar Buyukkahraman <basarbk@yahoo.com>
2021-10-07 23:18:23 +0300

    0305-input-style

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,18 @@
 const Input = (props) => {
   const { id, label, onChange, help } = props;
+
+  let inputClass = 'form-control';
+  if (help) {
+    inputClass += ' is-invalid';
+  }
+
   return (
     <div className="mb-3">
       <label htmlFor={id} className="form-label">
         {label}
       </label>
-      <input id={id} className="form-control" onChange={onChange} />
-      <span>{help}</span>
+      <input id={id} className={inputClass} onChange={onChange} />
+      <span className="invalid-feedback">{help}</span>
     </div>
   );
 };
---
+++ src/components/Input.spec.js
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import Input from './Input';
+
+it('has is-invalid class for input when help is set', () => {
+  const { container } = render(<Input help="Error message" />);
+  const input = container.querySelector('input');
+  expect(input.classList).toContain('is-invalid');
+});
+
+it('has invalid-feedback class for span when help is set', () => {
+  const { container } = render(<Input help="Error message" />);
+  const span = container.querySelector('span');
+  expect(span.classList).toContain('invalid-feedback');
+});
+
+it('does not have is-invalid class for input when help is not set', () => {
+  const { container } = render(<Input />);
+  const input = container.querySelector('input');
+  expect(input.classList).not.toContain('is-invalid');
+});
