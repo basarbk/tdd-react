00b6949991abfcec17a726a2e5bf20b69b63760c
Basar Buyukkahraman <basarbk@yahoo.com>
2021-10-16 23:20:20 +0300

    0802-navbar-component

--- src/App.js
+++ src/App.js
@@ -4,45 +4,19 @@ import LoginPage from './pages/LoginPage';
 import UserPage from './pages/UserPage';
 import AccountActivationPage from './pages/AccountActivationPage';
 import LanguageSelector from './components/LanguageSelector';
-import { useTranslation } from 'react-i18next';
-import logo from './assets/hoaxify.png';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { useState } from 'react';
+import NavBar from './components/NavBar';
 
 function App() {
   const [auth, setAuth] = useState({
     isLoggedIn: false,
     id: ''
   });
-  const { t } = useTranslation();
 
   return (
     <Router>
-      <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
-        <div className="container">
-          <Link className="navbar-brand" to="/" title="Home">
-            <img src={logo} alt="Hoaxify" width="60" />
-            Hoaxify
-          </Link>
-          <ul className="navbar-nav">
-            {!auth.isLoggedIn && (
-              <>
-                <Link className="nav-link" to="/signup">
-                  {t('signUp')}
-                </Link>
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </>
-            )}
-            {auth.isLoggedIn && (
-              <Link className="nav-link" to={`/user/${auth.id}`}>
-                My Profile
-              </Link>
-            )}
-          </ul>
-        </div>
-      </nav>
+      <NavBar auth={auth} />
       <div className="container pt-3">
         <Route exact path="/" component={HomePage} />
         <Route path="/signup" component={SignUpPage} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/hoaxify.png';
+import { useTranslation } from 'react-i18next';
+
+const NavBar = (props) => {
+  const { auth } = props;
+  const { t } = useTranslation();
+
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
+      <div className="container">
+        <Link className="navbar-brand" to="/" title="Home">
+          <img src={logo} alt="Hoaxify" width="60" />
+          Hoaxify
+        </Link>
+        <ul className="navbar-nav">
+          {!auth.isLoggedIn && (
+            <>
+              <Link className="nav-link" to="/signup">
+                {t('signUp')}
+              </Link>
+              <Link className="nav-link" to="/login">
+                {t('login')}
+              </Link>
+            </>
+          )}
+          {auth.isLoggedIn && (
+            <Link className="nav-link" to={`/user/${auth.id}`}>
+              My Profile
+            </Link>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
