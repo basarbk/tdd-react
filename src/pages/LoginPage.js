ffa3aa5f90938c83564a35d83a539206d43b2a12
Basar Buyukkahraman <basarbk@yahoo.com>
2021-10-16 17:07:59 +0300

    0702-interactions

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,12 @@
 import Input from '../components/Input';
+import { useState } from 'react';
+
 const LoginPage = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  let disabled = !(email && password);
+
   return (
     <div
       className="col-lg-6 offset-lg-3 col-md-8 offset-md-2"
@@ -10,10 +17,19 @@ const LoginPage = () => {
           <h1 className="text-center">Login</h1>
         </div>
         <div className="card-body">
-          <Input id="email" label="E-mail" />
-          <Input id="password" label="Password" type="password" />
+          <Input
+            id="email"
+            label="E-mail"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
           <div className="text-center">
-            <button className="btn btn-primary" disabled>
+            <button className="btn btn-primary" disabled={disabled}>
               Login
             </button>
           </div>
--- src/pages/LoginPage.spec.js
+++ src/pages/LoginPage.spec.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import LoginPage from './LoginPage';
+import userEvent from '@testing-library/user-event';
 
 describe('Login Page', () => {
   describe('Layout', () => {
@@ -32,6 +33,17 @@ describe('Login Page', () => {
       render(<LoginPage />);
       const button = screen.queryByRole('button', { name: 'Login' });
       expect(button).toBeDisabled();
+    });
+  });
+  describe('Interactions', () => {
+    it('enables the button when email and password inputs are filled', () => {
+      render(<LoginPage />);
+      const emailInput = screen.getByLabelText('E-mail');
+      const passwordInput = screen.getByLabelText('Password');
+      userEvent.type(emailInput, 'user100@mail.com');
+      userEvent.type(passwordInput, 'P4ssword');
+      const button = screen.queryByRole('button', { name: 'Login' });
+      expect(button).toBeEnabled();
     });
   });
 });
