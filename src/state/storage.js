bfab2b83ccaa9c9992955d79b21342daf979d1b4
Basar Buyukkahraman <basarbk@yahoo.com>
2021-10-17 23:27:42 +0300

    0812-encryption

--- src/state/storage.js
+++ src/state/storage.js
@@ -1,17 +1,13 @@
+import SecureLS from 'secure-ls';
+
+const secureLS = new SecureLS();
+
 const setItem = (key, value) => {
-  localStorage.setItem(key, JSON.stringify(value));
+  secureLS.set(key, value);
 };
 
 const getItem = (key) => {
-  const storedState = localStorage.getItem(key);
-  if (!storedState) {
-    return null;
-  }
-  try {
-    return JSON.parse(storedState);
-  } catch (error) {
-    return storedState;
-  }
+  return secureLS.get(key);
 };
 
 const clear = () => {
